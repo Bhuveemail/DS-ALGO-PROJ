e01c5cf92514ecbcc92a6233ca68f167af53c3e7
Bhuveemail <bhuveemail@gmail.com>
2024-03-03 20:55:58 -0500

    Commit my changes..

--- cucumber-testng-bdd/src/test/java/Runners/TestRunner.java
+++ cucumber-testng-bdd/src/test/java/Runners/TestRunner.java
@@ -24,7 +24,7 @@ import commonFunctions.Utility;
             	"json:target/cucumber-reports/cucumber.json",
             	"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
         		"html:target/cucumber-reports/cucumberreport.html"},
-        tags = "@test"
+        tags = "@signin_page_positive"
         		
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
